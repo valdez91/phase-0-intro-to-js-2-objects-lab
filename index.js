e9f66eacd6b4775bb33476c52bd13670cd7208f4
Victor Kemboi <vickemtoo97@gmail.com>
2024-03-22 09:36:04 +0300

    objects lab,calling obj

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+
+const employee = {name: 'Sam',
+streetAddress: '11 Broadway'
+};
+const updateEmployeeWithKeyAndValue = (employee, key, value) => {
+    
+    return Object.assign({}, employee, {[key]: value});
+}
+
+const destructivelyUpdateEmployeeWithKeyAndValue = (employee, key, value) => {
+    employee[key] = value;
+    return employee;
+}
+const deleteFromEmployeeByKey = (employee, key) => {
+    const newObj = Object.assign({}, employee);
+    delete newObj[key];
+    return newObj;
+}
+const destructivelyDeleteFromEmployeeByKey = (employee, key) => {
+    delete employee[key];
+    return employee;
+}
